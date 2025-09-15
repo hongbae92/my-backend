2a51d7f4c3ee9746e4a6f57bf901c9b3dfc9ad1b
hongbae929292@gmail.com <hongbae929292@gmail.com>
2025-09-15 10:58:20 +0900

    zzz

--- api/index.js
+++ api/index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 
 // DB 연결 (환경변수는 Vercel 대시보드에 등록)
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   // 포트 추가
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
 
 // Swagger 설정
 const options = {
